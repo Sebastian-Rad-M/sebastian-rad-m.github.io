67ac3c6f8f6b79800ecfb0817bde19d735ac2ff9
Sebastian-Rad-M <sebastian.mihail.raducu@gmail.com>
2026-03-03 10:18:09 +0200

    Refactor dark mode toggle and button event listeners

--- dark_mode.js
+++ dark_mode.js
@@ -5,24 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const btn = document.querySelector('.theme-toggle');
     
-    if (!btn) return;
+    if (btn) {
+        btn.addEventListener('click', function () {
+            document.body.classList.toggle('dark');
+            
+            const isDark = document.body.classList.contains('dark');
+            localStorage.setItem('theme', isDark ? 'dark' : 'light');
+        });
+    }
 
-    btn.addEventListener('click', function () {
-        document.body.classList.toggle('dark');
-        
-        const isDark = document.body.classList.contains('dark');
-        localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    });
-});
-
-const submitBtn = document.getElementById('emailBtn');
-
-if (submitBtn) {
-  submitBtn.addEventListener('mouseenter', () => {
-    const r = Math.floor(Math.random() * 50); 
-    const g = Math.floor(Math.random() * 50) + 100; 
-    const b = Math.floor(Math.random() * 50) + 100;
     
-    submitBtn.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  });
-}
+    const submitBtn = document.getElementById('emailBtn');
+
+    if (submitBtn) {
+      submitBtn.addEventListener('mouseenter', () => {
+        const r = Math.floor(Math.random() * 50); 
+        const g = Math.floor(Math.random() * 50) + 100; 
+        const b = Math.floor(Math.random() * 50) + 100;
+        
+        submitBtn.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      });
+    }
+});
