ca364494f0d13f5dca9a142a247a28649040260e
Raducu Sebastian <sebastian.mihail.raducu@gmail.com>
2026-07-17 13:03:32 +0300

    Rewrote in Astro

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection} from 'astro:content';
+ import { z } from 'astro/zod'; 
+import { glob } from 'astro/loaders'; 
+
+const essaysCollection = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/essays" }),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        featured: z.boolean().default(false),
+    }),
+});
+
+export const collections = {
+    'essays': essaysCollection,
+};
